d42d9145c8c78b3017ffe8c24fcd9423fb6726bd
Chris Allison <chrisallisonsax@gmail.com>
2023-10-04 18:53:38 -0400

    Knight move logic

--- src/game_logic/chess_pieces/Knight.cpp
+++ src/game_logic/chess_pieces/Knight.cpp
@@ -6,5 +6,17 @@ Knight::Knight(std::vector<int> square, Color pieceColor, ChessBoard* board) {
     movedYet = false;
 }
 bool Knight::isLegalMove(std::vector<int> square) {
-    
+    int pathLengthX = std::abs(square.at(0) - location.at(0));
+    int pathLengthY = std::abs(square.at(1) - location.at(1));
+    if (square == location) {
+        return false;
+    } else if (pieceBoard->getPieceAt(square)->getColor() == color) {
+        return false;
+    } else if (location.at(0) > 7 || location.at(0) < 0 || location.at(1) > 7 || location.at(1) < 0) {
+        return false;
+    } else if ((pathLengthX == 1 && pathLengthY == 2) || (pathLengthX == 2 && pathLengthY == 1)) {
+            return true;
+    } else {
+        return false;
+    }
 }
